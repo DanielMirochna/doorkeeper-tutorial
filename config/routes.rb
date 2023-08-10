860059256212abb12ec618e0339e4a0507fa758a
Daniel Mirochna <daniel.mirochna@visma.com>
2023-08-10 11:13:07 +0200

    Finish configuration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   end
   devise_for :users
   resources :bookmarks
+  namespace :api do
+    resources :users, only: %i[create]
+    resources :bookmarks, only: %i[index]
+  end
   root 'bookmarks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
